6713409da02fa1b32dc3bece552af387eec57f9d
José Espinoza <jose.espinoza.3d@gmail.com>
2024-02-11 19:18:04 -0500

    feat: get user info from root component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,23 @@ import "./index.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+import { useState, useEffect } from "react";
+import { api } from "./utils/api";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState({});
+
+  useEffect(() => {
+    api.getUserInfo().then((res) => {
+      setCurrentUser({
+        about: res.about,
+        avatar: res.avatar,
+        name: res.name,
+        id: res._id,
+      });
+    });
+  }, []);
+
   return (
     <div className="page page_theme_dark">
       <Header></Header>
--- src/utils/api.js
+++ src/utils/api.js
@@ -4,4 +4,9 @@ import { apiConfig } from "./config";
 const { address, groupId, token } = apiConfig;
 const api = apiHandler.getInstance({ address, groupId, token });
 
+api.getUserInfo = () => {
+  api.configRequest({ resource: "users/me" });
+  return api.get();
+};
+
 export { api };
